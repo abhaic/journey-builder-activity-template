ecc55c42937318f18ab57eedb9695f74d6047578
Matthew Berry <berrym@us.imshealth.com>
2019-10-30 15:22:59 -0400

    data

--- routes/data.js
+++ routes/data.js
@@ -1,46 +1,56 @@
-const columns = ["firstName", "lastName", "emailAddress", "phoneNumber"];
-
-const data = [
-  ["HACK201910301", "Rashi", "Kapil", "rakapil@us.imshealth.com", "7329100468"],
-  ["HACK201910302", "Rashi", "Kapil", "Rashi.kapil@outlook.com", "9736154533"],
-  ["HACK201910303", "Richa", "Gor", "rgor@us.imshealth.com", "7325939478"],
-  [
-    "HACK201910304",
-    "Matthew",
-    "Berry",
-    "berrym@us.imshealth.com",
-    "5703507242"
-  ],
-  [
-    "HACK201910305",
-    "Shelly",
-    "Dehaven",
-    "dehavens@us.imshealth.com",
-    "5702362949"
-  ],
-  [
-    "HACK201910306",
-    "Harish",
-    "Lakshmi",
-    "hlakshmi@us.imshealth.com",
-    "9086352722"
-  ],
-  [
-    "HACK201910307",
-    "Senthil",
-    "Namasivayam",
-    "snamasivayam@us.imshealth.com",
-    "9082855054"
-  ],
-  ["HACK201910308", "Ray", "Madara", "rmadara@us.imshealth.com", "7325404278"],
-  ["HACK201910309", "Ray", "Madara", "raymadara@gmail.com", "7327547498"]
-];
-
-exports.json = data.reduce((map, r) => {
-  const [key, ...rest] = r;
-  const values = rest.reduce((map, v, i) => {
-    const c = columns[i];
-    return { ...map, [c]: v };
-  }, {});
-  return { ...map, [key]: values };
-}, {});
+module.exports = {
+  HACK201910301: {
+    firstName: "Rashi",
+    lastName: "Kapil",
+    emailAddress: "rakapil@us.imshealth.com",
+    phoneNumber: "7329100468"
+  },
+  HACK201910302: {
+    firstName: "Rashi",
+    lastName: "Kapil",
+    emailAddress: "Rashi.kapil@outlook.com",
+    phoneNumber: "9736154533"
+  },
+  HACK201910303: {
+    firstName: "Richa",
+    lastName: "Gor",
+    emailAddress: "rgor@us.imshealth.com",
+    phoneNumber: "7325939478"
+  },
+  HACK201910304: {
+    firstName: "Matthew",
+    lastName: "Berry",
+    emailAddress: "berrym@us.imshealth.com",
+    phoneNumber: "5703507242"
+  },
+  HACK201910305: {
+    firstName: "Shelly",
+    lastName: "Dehaven",
+    emailAddress: "dehavens@us.imshealth.com",
+    phoneNumber: "5702362949"
+  },
+  HACK201910306: {
+    firstName: "Harish",
+    lastName: "Lakshmi",
+    emailAddress: "hlakshmi@us.imshealth.com",
+    phoneNumber: "9086352722"
+  },
+  HACK201910307: {
+    firstName: "Senthil",
+    lastName: "Namasivayam",
+    emailAddress: "snamasivayam@us.imshealth.com",
+    phoneNumber: "9082855054"
+  },
+  HACK201910308: {
+    firstName: "Ray",
+    lastName: "Madara",
+    emailAddress: "rmadara@us.imshealth.com",
+    phoneNumber: "7325404278"
+  },
+  HACK201910309: {
+    firstName: "Ray",
+    lastName: "Madara",
+    emailAddress: "raymadara@gmail.com",
+    phoneNumber: "7327547498"
+  }
+};
