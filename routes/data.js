02c23846be55fb8e0dd71eedfb82bc5e47e99dba
abhaic <abhaic@gmail.com>
2020-11-21 22:48:18 -0800

    Update data.js

--- routes/data.js
+++ routes/data.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HACK201910301: {
+  HACK202011211: {
     firstName: "Rashi",
-    lastName: "Kapil",
-    emailAddress: "rakapil@us.imshealth.com",
+    lastName: "Gupta",
+    emailAddress: "rashi.g5@gmail.com",
     phoneNumber: "7329100468"
   },
   HACK201910302: {
@@ -11,46 +11,4 @@ module.exports = {
     emailAddress: "Rashi.kapil@outlook.com",
     phoneNumber: "9736154533"
   },
-  HACK201910303: {
-    firstName: "Richa",
-    lastName: "Gor",
-    emailAddress: "rgor@us.imshealth.com",
-    phoneNumber: "7325939478"
-  },
-  HACK201910304: {
-    firstName: "Matthew",
-    lastName: "Berry",
-    emailAddress: "berrym@us.imshealth.com",
-    phoneNumber: "5703507242"
-  },
-  HACK201910305: {
-    firstName: "Shelly",
-    lastName: "Dehaven",
-    emailAddress: "dehavens@us.imshealth.com",
-    phoneNumber: "5702362949"
-  },
-  HACK201910306: {
-    firstName: "Harish",
-    lastName: "Lakshmi",
-    emailAddress: "hlakshmi@us.imshealth.com",
-    phoneNumber: "9086352722"
-  },
-  HACK201910307: {
-    firstName: "Senthil",
-    lastName: "Namasivayam",
-    emailAddress: "snamasivayam@us.imshealth.com",
-    phoneNumber: "9082855054"
-  },
-  HACK201910308: {
-    firstName: "Ray",
-    lastName: "Madara",
-    emailAddress: "rmadara@us.imshealth.com",
-    phoneNumber: "7325404278"
-  },
-  HACK201910309: {
-    firstName: "Ray",
-    lastName: "Madara",
-    emailAddress: "raymadara@gmail.com",
-    phoneNumber: "7327547498"
-  }
 };
