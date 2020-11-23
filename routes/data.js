be393c0228c0a409739127f66940465cbdf0abb7
abhaic <abhaic@gmail.com>
2020-11-23 07:29:11 -0800

    Update data.js

--- routes/data.js
+++ routes/data.js
@@ -6,9 +6,15 @@ module.exports = {
     phoneNumber: "17329100468"
   },
   HACK202011212: {
-    firstName: "Rashi",
-    lastName: "Kapil",
+    firstName: "Demo1",
+    lastName: "Demo1",
     emailAddress: "Rashi.kapil@outlook.com",
     phoneNumber: "19736154533"
+  },
+  HACK202011213: {
+    firstName: "Demo2",
+    lastName: "Demo2",
+    emailAddress: "rashi.g5@gmail.com",
+    phoneNumber: "919927017870"
   },
 };
