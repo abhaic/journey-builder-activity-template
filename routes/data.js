f6b6d67c59015c3bf3a60bee7310557443362a03
abhaic <abhaic@gmail.com>
2020-11-23 07:08:14 -0800

    Move ISD code to data

--- routes/data.js
+++ routes/data.js
@@ -3,12 +3,12 @@ module.exports = {
     firstName: "Rashi",
     lastName: "Gupta",
     emailAddress: "rashi.g5@gmail.com",
-    phoneNumber: "7329100468"
+    phoneNumber: "17329100468"
   },
   HACK201910302: {
     firstName: "Rashi",
     lastName: "Kapil",
     emailAddress: "Rashi.kapil@outlook.com",
-    phoneNumber: "9736154533"
+    phoneNumber: "19736154533"
   },
 };
